845828c1985bcfee2d3aa26636b79288f687956e
Milan Tomic <milan.tomic.rs@gmail.com>
2017-03-02 11:35:21 +0100

    working with adding github cards when inputing users

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
-var Button = React.createClass({
-    localHandleClick: function () {
-        this.props.localHandleClick(this.props.increment);
+var Card = React.createClass({
+    getInitialState: function () {
+        return {};
+    },
+    componentDidMount: function () {
+        var component = this;
+        $.get('https://api.github.com/users/' + this.props.login, function (data) {
+            component.setState(data);
+        });
     },
     render: function () {
         return React.createElement(
-            "button",
-            { onClick: this.localHandleClick },
-            "+",
-            this.props.increment
+            'div',
+            null,
+            React.createElement('img', { src: this.state.avatar_url, width: '80' }),
+            React.createElement(
+                'h3',
+                null,
+                this.state.name
+            ),
+            React.createElement('hr', null)
         );
     }
 });
 
-var Result = React.createClass({
+var Form = React.createClass({
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var loginInput = this.refs.login;
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+    },
     render: function () {
         return React.createElement(
-            "div",
-            null,
-            this.props.localCounter
+            'form',
+            { onSubmit: this.handleSubmit },
+            React.createElement('input', { placeholder: 'github login', ref: 'login' }),
+            React.createElement(
+                'button',
+                null,
+                'Add'
+            )
         );
     }
 });
 
 var Main = React.createClass({
     getInitialState: function () {
-        return { counter: 0 };
+        return { logins: [] };
     },
-    handleClick: function (increment) {
-        this.setState({ counter: this.state.counter + increment });
+    addCard: function (loginToAdd) {
+        this.setState({ logins: this.state.logins.concat(loginToAdd) });
     },
     render: function () {
+        var cards = this.state.logins.map(function (login) {
+            return React.createElement(Card, { login: login });
+        });
         return React.createElement(
-            "div",
+            'div',
             null,
-            React.createElement(Button, { localHandleClick: this.handleClick, increment: 1 }),
-            React.createElement(Button, { localHandleClick: this.handleClick, increment: 5 }),
-            React.createElement(Button, { localHandleClick: this.handleClick, increment: 10 }),
-            React.createElement(Button, { localHandleClick: this.handleClick, increment: 100 }),
-            React.createElement(Result, { localCounter: this.state.counter })
+            React.createElement(Form, { addCard: this.addCard }),
+            cards
         );
     }
 });
 
-ReactDOM.render(React.createElement(Main, null), document.getElementById("root"));
+ReactDOM.render(React.createElement(Main, null), document.getElementById('root'));
--- script.jsx
+++ script.jsx
@@ -1,41 +1,59 @@
-var Button = React.createClass({
-    localHandleClick: function() {
-        this.props.localHandleClick(this.props.increment);
+var Card = React.createClass({
+    getInitialState: function() {
+        return {};
+    },
+    componentDidMount: function() {
+        var component = this;
+        $.get('https://api.github.com/users/' + this.props.login, function(data) {
+            component.setState(data);
+        })
     },
     render: function() {
         return (
-            <button onClick={this.localHandleClick}>+{this.props.increment}</button>
-        )
+            <div>
+                <img src={this.state.avatar_url} width="80" />
+                <h3>{this.state.name}</h3>
+                <hr/>
+            </div>
+        );
     }
 });
 
-var Result = React.createClass({
+var Form = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var loginInput = this.refs.login;
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+    },
     render: function() {
         return (
-            <div>{this.props.localCounter}</div>
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="github login" ref="login"/>
+                <button>Add</button>
+            </form>
         )
     }
 });
 
 var Main = React.createClass({
-        getInitialState: function() {
-        return {counter: 0};
+    getInitialState: function() {
+        return {logins: []};
     },
-        handleClick: function(increment) {
-        this.setState({ counter: this.state.counter+increment });
+    addCard: function(loginToAdd) {
+        this.setState({logins: this.state.logins.concat(loginToAdd)});
     },
     render: function() {
+        var cards = this.state.logins.map(function(login) {
+            return (<Card login={login} />);
+        });
         return (
             <div>
-                <Button localHandleClick={this.handleClick} increment={1}/>
-                <Button localHandleClick={this.handleClick} increment={5}/>
-                <Button localHandleClick={this.handleClick} increment={10}/>
-                <Button localHandleClick={this.handleClick} increment={100}/>
-                <Result localCounter={this.state.counter}/>
+                <Form addCard={this.addCard} />
+                {cards}
             </div>
         )
     }
 });
 
-
-ReactDOM.render(<Main />, document.getElementById("root"));
+ReactDOM.render( <Main />, document.getElementById('root'));
